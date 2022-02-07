c12140f732783a984537a7e8898230349c68b6ee
Wallisson Marinho <wallissonmarinho@hotmail.com>
2022-02-07 18:22:26 -0300

    add mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
-  imports: [TweetsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DSN, {
+      useNewUrlParser: true,
+    }),
+    TweetsModule,
+  ],
 })
 export class AppModule {}
