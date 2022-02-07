784d2adda063ccb6feafcbf65cb41908a8393b34
Wallisson Marinho <wallissonmarinho@hotmail.com>
2022-02-07 17:22:17 -0300

    add docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TweetsModule],
 })
 export class AppModule {}
